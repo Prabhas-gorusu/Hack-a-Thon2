3c8535e4bfaa93f7dd2342b8eb9cfd88eb3a0e52
Prabhas_gorusu <236K1A4225@idealtech.edu.in>
2025-09-08 08:25:27 +0530

    Delete src directory

--- src/ai/dev.ts
+++
@@ -1,7 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/suggest-crops.ts';
-import '@/ai/flows/suggest-pesticides.ts';
-import '@/ai/flows/diagnose-plant-health.ts';
-import '@/ai/flows/get-crop-details.ts';
